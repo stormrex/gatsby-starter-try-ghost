c2d75d3ef7048328a7203f32e1454ea496737f24
StorM <78651968+stormrex@users.noreply.github.com>
2021-03-28 08:23:17 +0500

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     // Do not include a trailing slash!
-    siteUrl: `https://stormyghost.gatsby.io`,
+    siteUrl: `https://stormyghostz.gatsby.io`,
 
     // Enable infinite scroll (default: true)
     infiniteScroll: true,
